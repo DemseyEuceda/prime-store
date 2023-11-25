e7726230be424d96198e342a86875dda8c2252b6
imer813 <73193682+imer813@users.noreply.github.com>
2023-11-25 08:43:50 -0600

    Estilos a Pantalla Login
    Se le dio estilos a la pantalla login

--- src/componentes/card/Login.jsx
+++ src/componentes/card/Login.jsx
@@ -1,25 +1,36 @@
-const CardLogin = () =>{
+import React from "react";
 
-return(
+const CardLogin = ({ setShowNavbar }) => {
+  return (
+    <div className="flex h-screen">
+      <div className=" flex h-screen w-full bg-gray-500 h-full">
+            
+      </div>
+      <div className="container-login flex h-screen justify-center items-center rounded w-full bg-gray h-full">
+        <h1 className="title-login text-black flex-1">Iniciar Sesión</h1>
+        <div className="bg-gray-200 flex flex-col items-center main-content mt-auto mt-4 mb-4 rounded">
+          <label className="label-input-login text-miColorAzul">Email:</label>
+          <input type="email" className="border border-black rounded p-2" />
 
-    <div className="container-login flex flex-col items-center h-200 w-200 rounded" >
-        <h1 className="title-login text-black flex-1" >Iniciar Sesión</h1>
-         <div className="bg-gray-200 flex flex-col items-center main-content mt-auto mt-4 mb-4 rounded">
-         <label className="label-input-login text-miColorAzul" >Email:</label>
-                <input type="e-mail border border-black rounded p-2"/>
+          <label className="label-input-login text-miColorAzul">
+            Contraseña
+          </label>
+          <input type="password" className="border border-black rounded p-2" />
 
-                <label className="label-input-login text-miColorAzul" >Contraseña</label>
-                <input type="password border border-black rounded p-2"/>
-        
-                <button className="sig-in text-white bg-miColorAzulOscuro rounded" >Iniciar Sesión</button>
+          <button
+            className="sig-in text-white bg-miColorAzulOscuro rounded"
+            onClick={() => setShowNavbar(true)}
+          >
+            Iniciar Sesión
+          </button>
 
-                <button className="create-account text-white bg-miColorVerde rounded "  >Crear cuenta nueva</button> 
-
-         </div>
-                      
+          <button className="create-account text-white bg-miColorVerde rounded">
+            Crear cuenta nueva
+          </button>
+        </div>
+      </div>
     </div>
-
-)
-}
+  );
+};
 
 export default CardLogin;
