49770275d1c91eeaa9ebd1b742b521282c261edd
DemseyEuceda <50280205+DemseyEuceda@users.noreply.github.com>
2023-11-29 20:31:18 -0600

    agregar  firebase al proyecto

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyBmRv0bmv62-M9z75ZxHmdMayu97JTpxHA",
+    authDomain: "prime-store-4e259.firebaseapp.com",
+    databaseURL: "https://prime-store-4e259-default-rtdb.firebaseio.com",
+    projectId: "prime-store-4e259",
+    storageBucket: "prime-store-4e259.appspot.com",
+    messagingSenderId: "167788427098",
+    appId: "1:167788427098:web:e781d7f9675cc06ac4e563",
+    measurementId: "G-7QR03ZBG8B"
+    }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FirebaseAppProvider } from 'reactfire';
+import firebaseConfig from './firebase-config';
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <Suspense fallback={'Cargando...'} >
     <App />
+    </Suspense>
+    </FirebaseAppProvider>
   </React.StrictMode>
 );
 
