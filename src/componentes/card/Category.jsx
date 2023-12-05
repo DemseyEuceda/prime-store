fd32e1632a5797f49d2bd1e374d43176eedea6f0
DemseyEuceda <50280205+DemseyEuceda@users.noreply.github.com>
2023-12-04 21:25:13 -0600

    se agregó imagenes y firebase a las categorias

--- src/componentes/card/Category.jsx
+++ src/componentes/card/Category.jsx
@@ -7,7 +7,7 @@ const CardCategory = ({ category }) => {
       className="group relative p-6 border border-red-300 bg-gray-1 rounded-lg"
     >
       <div className="mb-1">
-        <p className="text-black font-bold">Videojuegos</p>
+        <p className="text-black font-bold">{category.nombre}</p>
       </div>
       <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
         <img
--- src/pages/Categorias.jsx
+++ src/pages/Categorias.jsx
@@ -1,131 +1,33 @@
-import CardCategory from "../componentes/card/Category"
+import CardCategory from "../componentes/card/Category";
+import { db } from "../firebase-config";
+import { collection, getDocs } from "firebase/firestore";
+
+import { useEffect, useState } from "react";
 
 
-// const products = [
-//   {
-//     id: 1,
-//     name: "Basic Tee",
-//     href: "#",
-//     imageSrc:
-//       "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-//     imageAlt: "Front of men's Basic Tee in black.",
-//     price: "$35",
-//     color: "Black",
-//   },
-//   {
-//     id: 2,
-//     name: "Basic Tee",
-//     href: "#",
-//     imageSrc:
-//       "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-//     imageAlt: "Front of men's Basic Tee in black.",
-//     price: "$35",
-//     color: "Black",
-//   },
-//   {
-//     id: 3,
-//     name: "Basic Tee",
-//     href: "#",
-//     imageSrc:
-//       "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-//     imageAlt: "Front of men's Basic Tee in black.",
-//     price: "$35",
-//     color: "Black",
-//   },
-//   {
-//     id: 4,
-//     name: "Basic Tee",
-//     href: "#",
-//     imageSrc:
-//       "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-//     imageAlt: "Front of men's Basic Tee in black.",
-//     price: "$35",
-//     color: "Black",
-//   },
-//   {
-//     id: 5,
-//     name: "Basic Tee",
-//     href: "#",
-//     imageSrc:
-//       "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-//     imageAlt: "Front of men's Basic Tee in black.",
-//     price: "$35",
-//     color: "Black",
-//   },
-//   {
-//     id: 6,
-//     name: "Basic Tee",
-//     href: "#",
-//     imageSrc:
-//       "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-//     imageAlt: "Front of men's Basic Tee in black.",
-//     price: "$35",
-//     color: "Black",
-//   },
-//   {
-//     id: 7,
-//     name: "Basic Tee",
-//     href: "#",
-//     imageSrc:
-//       "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-//     imageAlt: "Front of men's Basic Tee in black.",
-//     price: "$35",
-//     color: "Black",
-//   },
-//   {
-//     id: 8,
-//     name: "Basic Tee",
-//     href: "#",
-//     imageSrc:
-//       "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-//     imageAlt: "Front of men's Basic Tee in black.",
-//     price: "$35",
-//     color: "Black",
-//   },
-//   {
-//     id: 9,
-//     name: "Basic Tee",
-//     href: "#",
-//     imageSrc:
-//       "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-//     imageAlt: "Front of men's Basic Tee in black.",
-//     price: "$35",
-//     color: "Black",
-//   },
-//   {
-//     id: 10,
-//     name: "Basic Tee",
-//     href: "#",
-//     imageSrc:
-//       "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-//     imageAlt: "Front of men's Basic Tee in black.",
-//     price: "$35",
-//     color: "Black",
-//   },
-//   // More products...
-// ];
 
-const categorias = [
-  {
-    id: 1,
-    name: "juegos",
-    href: "/products/juegos",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-01.jpg",
-    imageAlt: "Models sitting back to back, wearing Basic Tee in black and bone.",
-  },
-  {
-    id: 2,
-    name: "Telefonos",
-    href: "/products/telefonos",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-01.jpg",
-    imageAlt: "Models sitting back to back, wearing Basic Tee in black and bone.",
-  },
-];
+
 
 
 export default function Home() {
+  const [categorias, setCategorias] = useState([])
+
+  useEffect(()=>{
+    const ref = collection(db, "categorias");
+    var list = []
+    getDocs(ref).then((res)=>{
+      res.docs.map((doc)=>{
+        list.push(doc.data());        
+      })
+      setCategorias(list);
+    })
+  
+    
+  
+  }, [])
+
+
+
   return (
     <div className="bg-white border border-yellow-300">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-6 lg:max-w-7xl lg:px-8 border border-green-300">
