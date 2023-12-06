7c10484c92bc74e4969f37080ce49bc4c3f9e0db
DemseyEuceda <50280205+DemseyEuceda@users.noreply.github.com>
2023-12-06 12:56:57 -0600

    se movio el sidebar a la derecha

--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -70,7 +70,7 @@ function Navbar() {
 
       <div className={`${!open && "hidden"} bg-gray-600/50 min-h-screen w-full fixed top-0 left-0 right-0 backdrop-blur-sm`} onClick={() => setOpen(false)}></div>
 
-      <div className={`${open ? "w-80" : "w-0"}  bg-purple-1 min-h-screen fixed top-0 left-0 transition-all duration-300`}>
+      <div className={`${open ? "w-80" : "w-0"}  bg-purple-1 min-h-screen fixed top-0 right-0 transition-all duration-300`}>
         <div className={`${!open && "hidden"} pt-3`}>
           <button className='ml-4 text-white mb-14' onClick={() => setOpen(false)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
